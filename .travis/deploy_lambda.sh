0022684e26e7bb9b5fd35778f5ceaf073ab5243c
waheng chang <wahengchang@gmail.com>
2019-06-28 17:12:13 +0800

    no message

--- .travis/deploy_lambda.sh
+++ .travis/deploy_lambda.sh
@@ -1,13 +1,13 @@
-echo "-=-=-=-=-=-=-=-= deploy_lambda"
-ls
-
 LAMBDA_PATH=$1
 SRC=src/
 LAMBDA_NAME=${LAMBDA_PATH#${SRC}}
 LAMBDA_NAME=${LAMBDA_NAME%/}
 
 echo Deploy $LAMBDA_NAME inside $LAMBDA_PATH
+pip install -r $LAMBDA_PATH"requirements.txt" -t $LAMBDA_PATH --upgrade
 
 cd $LAMBDA_PATH
-# zip -r $LAMBDA_NAME.zip .
+echo "[INFO] ziping: $LAMBDA_NAME"
+zip -r $LAMBDA_NAME.zip .
+echo "[INFO] deploy to aws: $LAMBDA_NAME"
 aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file fileb://$LAMBDA_NAME.zip
