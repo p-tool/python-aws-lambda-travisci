28b4c3745b966b4c65823c8a3ed1d7d320bb2d84
waheng chang <wahengchang@gmail.com>
2019-06-28 17:02:55 +0800

    no message

--- .travis/deploy_lambda.sh
+++ .travis/deploy_lambda.sh
@@ -6,8 +6,8 @@ SRC=src/
 LAMBDA_NAME=${LAMBDA_PATH#${SRC}}
 LAMBDA_NAME=${LAMBDA_NAME%/}
 
-echo Building $LAMBDA_NAME inside $LAMBDA_PATH
+echo Deploy $LAMBDA_NAME inside $LAMBDA_PATH
 
 cd $LAMBDA_PATH
 # zip -r $LAMBDA_NAME.zip .
-aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file $LAMBDA_NAME.zip
+aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file fileb://$LAMBDA_NAME.zip
